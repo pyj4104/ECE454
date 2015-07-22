6b53ca714aa1e3d577b212136c9a3764a03c5cee
hhuang25 <h53huang@sandbox.hortonworks.com>
2015-07-22 07:39:01 +0000

    part 3 very inefficient. doesn't work on full scale

--- assignment3/part3/src/Part3.java
+++ assignment3/part3/src/Part3.java
@@ -1,12 +1,13 @@
 
 import java.io.IOException;
 import java.util.StringTokenizer;
-
+import java.util.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -26,12 +27,50 @@ public class Part3 {
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
 		//StringTokenizer itr = new StringTokenizer(value.toString(),",");
-		String[] values = value.toString().split(",");
-		mw.put(new Text(values[0]),new ArrayWritable(Arrays.copyOfRange(values,1,values.length)));
-		context.write(one,mw);
+		if(value == null || value.toString().equals("")){
+            //System.out.println("empty");
+            return;
+        }
+        String[] values = value.toString().split(",");
+        if(values.length < 2){ 
+            //System.out.println("too short, length is " + values.length);
+            return;
+        }
+        //System.out.printf("Start map\nfirst item: %s, second item: %s\n",values[0],values[1]);
+        TextArrayWritable taw = new TextArrayWritable(Arrays.copyOfRange(values,1,values.length));
+		//System.out.printf("txtarrwritable: %s\n",taw.toString());
+        mw.put(new Text(values[0]),taw);
+		//System.out.println("put into mapwritable");
+        context.write(one,mw);
+        //System.out.println("written to context");
     }
   }
-  
+  public static class TextArrayWritable extends ArrayWritable {
+        public TextArrayWritable() {
+            super(Text.class);
+        }
+
+        public TextArrayWritable(String[] strings) {
+            super(Text.class);
+            Text[] texts = new Text[strings.length];
+            for (int i = 0; i < strings.length; i++) {
+                texts[i] = new Text(strings[i]);
+            }
+            set(texts);
+        }
+
+        public String toString(){
+            StringBuilder sb = new StringBuilder();
+            String prefix = "";
+            for(String s : super.toStrings()){
+                sb.append(prefix);
+                prefix = ",";
+                sb.append(s);
+            }
+            return sb.toString();
+        }
+    }
+   
   public static class GeneReducer 
        extends Reducer<IntWritable,MapWritable,Text,DoubleWritable> {
     private DoubleWritable result = new DoubleWritable();
@@ -39,21 +78,32 @@ public class Part3 {
     public void reduce(IntWritable key, Iterable<MapWritable> values, 
 	    Context context
 	    ) throws IOException, InterruptedException {
-		ArrayList<MapWritable> cache = new ArrayList<MapWritable>();
-		for(MapWritable mw : values){
-			cache.add(new MapWritable(mw.get()));
-		}
-		
-		for(int i = 0; i < cache.size(); i++){
+		//System.out.println("Start reduce");
+        ArrayList<MapWritable> cache = new ArrayList<MapWritable>();
+		Iterator<MapWritable> it = values.iterator();
+        while(it.hasNext()){
+            MapWritable mw = new MapWritable(it.next());
+            for(Map.Entry entry : mw.entrySet()){
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }    
+            cache.add(mw);
+        }
+        	
+		for(int i = 0; i < cache.size()-1; i++){
 			for(int j = i+1; j < cache.size(); j++){
-				Map.Entry<Text,ArrayWritable> entry = cache[i].entrySet().iterator().next();
-				Map.Entry<Text,ArrayWritable> entry2 = cache[j].entrySet().iterator().next();
+				Map.Entry entry = cache.get(i).entrySet().iterator().next();
+				Map.Entry entry2 = cache.get(j).entrySet().iterator().next();
 				double sum = 0.0;
-				for(int k = 0; k < entry.getValue().length; k++){
-					sum += Double.parseDouble(entry.getValue()[k]) + Double.parseDouble(entry2.getValue()[k]);
+                String[] entryStr = ((TextArrayWritable)entry.getValue()).toStrings();
+                String[] entryStr2 = ((TextArrayWritable)entry2.getValue()).toStrings();
+                //System.out.printf("first item:%s, second item:%s\n",Arrays.toString(entryStr),Arrays.toString(entryStr2));
+				for(int k = 0; k < entryStr.length; k++){
+                    //System.out.printf("%s * %s\n",entryStr[k],entryStr2[k]);
+					sum += Double.parseDouble(entryStr[k]) * Double.parseDouble(entryStr2[k]);
+                    //System.out.println("sum is " + sum);
 				}
 				result.set(sum);
-				context.write(new Text(entry.toString() + "," + entry2.toString()),result);
+				context.write(new Text(entry.getKey().toString() + "," + entry2.getKey().toString()),result);
 			}
 		}
 		
@@ -74,8 +124,8 @@ public class Part3 {
     job.setMapperClass(TokenizerMapper.class);
     //job.setCombinerClass(GeneReducer.class);
     job.setReducerClass(GeneReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(DoubleWritable.class);
+    job.setOutputKeyClass(IntWritable.class);
+    job.setOutputValueClass(MapWritable.class);
     FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
     FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
